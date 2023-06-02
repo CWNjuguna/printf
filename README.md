8fafad1ae15a789e672b61a828679a35ede8b15d
CWNjuguna <missnjugz@gmail.com>
2023-06-02 08:00:09 -0700

    main.h

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-- This is a group project done by Catherine & Lucy. This is a readme file on the printf tasks:
+- This is a group project done by Catherine & Lucy. This  readme file contains the printf tasks shown below:
 0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life
 - Write a function that produces output according to a format given.
 1. Education is when you read the fine print. Experience is what you get if you don't.
--- main.h
+++ main.h
@@ -7,44 +7,46 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* FLAGS */
+/**
+ *flags
+ */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES */
+/** 
+ *sizeS
+ */
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
  * struct fmt - Struct op
- * @fmt: The format.
- * @fn: The function associated.
+ * @fmt: this is the format
+ * @fn: this is the function associated
  */
 struct fmt
 {
 	char fmt;
-
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 /**
  * typedef struct fmt fmt_t - Struct op
- * @fmt: The format.
- * @fm_t: The function associated.
+ * @fmt: this is the format
+ * @fm_t: this is the function associated
  */
+
 typedef struct fmt fmt_t;
-
 int _printf(const char *format, ...);
-
 int handle_print(const char *fmt, int *i,
-
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
-/* Funtions to print chars and strings */
+/* 
+ * below are funtions used to print chars and strings
+ */
 int print_char(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
@@ -57,7 +59,9 @@ int print_percent(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
 
-/* Functions to print numbers */
+/* 
+ * below are the function used to print numbers
+ */
 int print_int(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
@@ -86,17 +90,26 @@ int print_hexa(va_list types, char map_to[],
 
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
+/*
+ * below are the functions used to print non printable characters
+ */
+
 int print_non_printable(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/*
+ *below are the functions used  to print memory address
+ */
+
 int print_pointer(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/*
+ * below are the functions used to handle other specifiers
+ */
+
 int get_flags(const char *format, int *i);
 
 int get_width(const char *format, int *i, va_list list);
@@ -105,17 +118,26 @@ int get_precision(const char *format, int *i, va_list list);
 
 int get_size(const char *format, int *i);
 
-/*Function to print string in reverse*/
+/*
+ * below are the functions used to print string(s) in reverse
+ */
+
 int print_reverse(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
 
-/*Function to print a string in rot 13*/
+/*
+ * below is the function used to print  a string in rot 13
+ */
+
 int print_rot13string(va_list types, char buffer[],
 
 	int flags, int width, int precision, int size);
 
-/* width handler */
+/*
+ * he function below is a width handler 
+ */
+
 int handle_write_char(char c, char buffer[],
 
 	int flags, int width, int precision, int size);
@@ -138,15 +160,12 @@ char buffer[],
 
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+/* utils
+ */
+
 int is_printable(char);
-
 int append_hexa_code(char, char[], int);
-
 int is_digit(char);
-
 long int convert_size_number(long int num, int size);
-
 long int convert_size_unsgnd(unsigned long int num, int size);
-
-#endif /* MAIN_H */
+#endif 
